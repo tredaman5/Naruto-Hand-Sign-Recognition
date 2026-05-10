d7fb5ad44e4e4ae1c3abc81fdeda5103c2967c3b
tredaman5 <smithtre1076@gmail.com>
2026-05-10 13:27:32 -0400

    adding code to naruto_signs.py

---
+++ src/labels/naruto_signs.py
@@ -0,0 +1,17 @@
+NARUTO_SIGNS = [
+    "tiger",
+    "serpent",
+    "boar",
+    "ram",
+    "horse",
+    "dog",
+]
+
+SIGN_DISPLAY_NAMES = {
+    "tiger": "Tiger",
+    "serpent": "Serpent",
+    "boar": "Boar",
+    "ram": "Ram",
+    "horse": "Horse",
+    "dog": "Dog",
+}
